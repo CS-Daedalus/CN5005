bb35ea6e33e41f08963c054ded32db4082c6a043
BuildTools <unconfigured@null.spigotmc.org>
2021-11-19 15:24:12 +0200

    Initial project structure

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends createWindow {
+
+    @Override
+    public void start(Stage mainStage) throws IOException {
+        super.start(mainStage);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
