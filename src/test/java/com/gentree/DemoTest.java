8c4757d6e5324b66104db6f5cf2bb92912fd5919
Panagiotis <400362+panosru@users.noreply.github.com>
2021-11-20 17:25:37 +0200

    Fixed testing environment

--- src/test/java/com/gentree/DemoTest.java
+++ src/test/java/com/gentree/DemoTest.java
@@ -1,21 +1,15 @@
+package com.gentree;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class DemoTest
 {
     @Test
     @DisplayName("Test 1")
     void demoTestMethod()
-    {
-        fail();
-    }
-
-    @Test
-    @DisplayName("Test 2")
-    void demoTestMethod2()
     {
         assertTrue(true);
     }
