67e881e2963a855ae96348df7f8d296f9d9b1e2f
Panagiotis <400362+panosru@users.noreply.github.com>
2021-11-20 03:49:38 +0200

    JDK 8 support

---
+++ src/main/java/com/gentree/App.java
@@ -0,0 +1,40 @@
+package com.gentree;
+
+import com.gentree.common.Util;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class App
+    extends Application
+{
+    @Override
+    public void start(Stage stage)
+        throws Exception
+    {
+        if (!(Util.isWindows() || Util.isMac()))
+        {
+            //Load Arial font for all *nix platforms
+            Font.loadFont(
+                Objects.requireNonNull(
+                    getClass().getClassLoader().getResource("com/gentree/fonts/Arial.ttf")).toExternalForm(), 12);
+        }
+
+        Parent root = FXMLLoader.load(
+            Objects.requireNonNull(
+                getClass().getClassLoader().getResource("com/gentree/view/main.fxml")));
+
+        stage.setTitle(Util.TITLE);
+        stage.setScene(new Scene(root, Util.APP_WIDTH, Util.APP_HEIGHT));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/gentree/controller/MainController.java
@@ -0,0 +1,49 @@
+package com.gentree.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController
+{
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    public void initialize()
+    {
+        welcomeText.setText("Hello World!");
+    }
+
+    @FXML
+    protected void importCsv(ActionEvent event)
+    {
+        System.out.println("Importing csv...");
+    }
+
+    @FXML
+    protected void sortPersons(ActionEvent event)
+    {
+        System.out.println("Sorting persons...");
+    }
+
+    @FXML
+    protected void findRelations(ActionEvent event)
+    {
+        System.out.println("Find relations...");
+    }
+
+    @FXML
+    protected void exportToDot(ActionEvent event)
+    {
+        System.out.println("Exporting DOT...");
+    }
+
+    @FXML
+    protected void exportToImage(ActionEvent event)
+    {
+        System.out.println("Exporting image...");
+    }
+
+}
