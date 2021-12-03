2fc4cedb691a69a783a75bcd8da5b814109f6eb0
BuildTools <unconfigured@null.spigotmc.org>
2021-12-03 20:28:02 +0200

    Minor adjustments to formatting

--- src/main/java/com/gentree/controller/RelationsController.java
+++ src/main/java/com/gentree/controller/RelationsController.java
@@ -4,17 +4,13 @@ package com.gentree.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class RelationsController
 {
@@ -31,45 +27,23 @@ public class RelationsController
     @FXML
     private Button myButton;
 
-    @FXML
-    private AnchorPane anchorPane;
-
     private List<String> stringListA = new ArrayList<>(5);
-
     private List<String> stringListB = new ArrayList<>(5);
 
     private ObservableList observableListA = FXCollections.observableArrayList();
-
-    private ObservableList observableListB = FXCollections.observableArrayList();
+    private ObservableList observableListB = FXCollections.observableArrayList(stringListB);
 
     public void setListView() {
 
         stringListA.add("PersonA");
-
         observableListA.setAll(stringListA);
-
         listViewA.setItems(observableListA);
 
-        AnchorPane.setLeftAnchor(listViewA, 65.0);
-
         stringListB.add("PersonB");
-
         observableListB.setAll(stringListB);
-
         listViewB.setItems(observableListB);
-
-        AnchorPane.setRightAnchor(listViewB, 30.0);
-
-        AnchorPane.setTopAnchor(myLabel, 5.0);
-
-        AnchorPane.setBottomAnchor(myButton, 5.0);
-
     }
 
     @FXML
-    void initialize(){
-
-        setListView();
-    }
-
+    void initialize() { setListView(); }
 }
