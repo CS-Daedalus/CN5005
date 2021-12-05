7c136fcfab91c3b2f38df14bf23e5f8e3352f36e
Panagiotis <400362+panosru@users.noreply.github.com>
2021-12-06 01:14:58 +0200

    Added tests for Person model and Util

--- src/main/java/com/gentree/model/Person.java
+++ src/main/java/com/gentree/model/Person.java
@@ -6,7 +6,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,16 +17,16 @@ public class Person
     private Person mother;
     private Person father;
 
-    public Person(String fullName, String gender, Person parent1, Person parent2)
+    public Person(String fullName, String gender, Person mother, Person father)
     {
-        this(fullName, gender, parent1);
-        setParent(parent2);
+        this(fullName, gender, mother);
+        setFather(father);
     }
 
-    public Person(String fullName, String gender, Person parent)
+    public Person(String fullName, String gender, Person mother)
     {
         this(fullName, gender);
-        setParent(parent);
+        setMother(mother);
     }
 
     public Person(String fullName, String gender)
@@ -36,69 +35,122 @@ public class Person
         setGender(gender);
     }
 
+    /**
+     * Returns the full name of this person
+     * @return the full name of this person
+     */
     public String getFullName()
     {
         return fullName;
     }
 
+    /**
+     * Sets the full name for this person
+     * @param fullName the full name of this person
+     */
     public void setFullName(String fullName)
     {
         this.fullName = Util.capitalise(fullName);
     }
 
-    public List<Person> getChildren()
-    {
-        return children;
-    }
-
-    public void addChild(@NotNull Person child)
-    {
-        child.setFather(this);
-        this.children.add(child);
-    }
-
+    /**
+     * Returns the gender of this person
+     * @return the gender os this person
+     */
     public Gender getGender()
     {
         return gender;
     }
 
+    /**
+     * Sets the gender of this person
+     * @param gender the gender of this person
+     */
     public void setGender(String gender)
     {
         this.gender = Gender.resolveGender(gender);
     }
 
+    /**
+     * Returns the mother of this person
+     * @return the mother of this person
+     */
     public Person getMother()
     {
         return mother;
     }
 
+    /**
+     * Sets the mother of this person
+     * @param mother the mother to set
+     */
     public void setMother(Person mother)
     {
+        // Prevent adding self as mother
+        preventSelf(mother);
+
         this.mother = mother;
     }
 
+    /**
+     * Returns the father of this person
+     * @return the father of this person
+     */
     public Person getFather()
     {
         return father;
     }
 
+    /**
+     * Sets the father of this person
+     * @param father the father to set
+     */
     public void setFather(Person father)
     {
+        // Prevent adding self as father
+        preventSelf(father);
+
         this.father = father;
     }
 
-    private void setParent(@NotNull Person parent)
+    /**
+     * Returns the children of this person
+     * @return the children of this person
+     */
+    public List<Person> getChildren()
     {
-        switch (parent.gender)
-        {
-            case WOMAN:
-                setMother(parent);
-                break;
+        return children;
+    }
 
-            case MAN:
-                setFather(parent);
-                break;
-        }
+    /**
+     * Adds a child to this person
+     * @param child the child to add
+     */
+    public void addChild(@NotNull Person child)
+    {
+        // Prevent adding self as child
+        preventSelf(child);
+
+        // Prevent adding same child twice
+        if (children.contains(child))
+            throw new IllegalArgumentException(
+                    String.format(
+                        Locale.getDefault(), "Person %s already has child %s", this.fullName, child.fullName));
+
+        child.setFather(this);
+        this.children.add(child);
+    }
+
+    /**
+     * Prevents adding self as parent of self
+     * @param person the person to check against
+     */
+    private void preventSelf(Person person)
+    {
+        if (this.equals(person))
+            throw new IllegalArgumentException(
+                    String.format(
+                        Locale.getDefault(), "Person %s cannot be a parent of itself", this.fullName));
     }
 
     /**
--- src/test/java/com/gentree/model/PersonTest.java
+++ src/test/java/com/gentree/model/PersonTest.java
@@ -1,4 +1,149 @@
 package com.gentree.model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PersonTest
-{}
+{
+    private final Person stub = new Person("uNkNoWn dOe", "woman");
+
+    @Test
+    @DisplayName("Test 4 params constructor")
+    public void testFourParamsConstructor()
+    {
+        Person mother = new Person("Female Doe", "woman");
+        Person father = new Person("Father Doe", "man");
+        Person person = new Person("John Doe", "man", mother, father);
+
+        assertEquals(mother, person.getMother());
+        assertEquals(father, person.getFather());
+        assertEquals("John Doe", person.getFullName());
+        assertEquals(Person.Gender.MAN, person.getGender());
+    }
+
+    @Test
+    @DisplayName("Test 3 params constructor")
+    public void testThreeParamsConstructor()
+    {
+        Person mother = new Person("Female Doe", "woman");
+        Person person = new Person("John Doe", "man", mother);
+
+        assertEquals(mother, person.getMother());
+        assertNull(person.getFather());
+        assertEquals("John Doe", person.getFullName());
+        assertEquals(Person.Gender.MAN, person.getGender());
+    }
+
+    @Test
+    @DisplayName("Test full name")
+    public void testGetFullName()
+    {
+        assertEquals("Unknown Doe", stub.getFullName());
+    }
+
+    @Test
+    @DisplayName("Test setting full name")
+    public void testSetFullName()
+    {
+        stub.setFullName("john doe");
+
+        assertEquals("John Doe", stub.getFullName());
+    }
+
+    @Test
+    @DisplayName("Test get gender")
+    public void testGetGender()
+    {
+        assertEquals(Person.Gender.WOMAN, stub.getGender());
+    }
+
+    @Test
+    @DisplayName("Test setting gender")
+    public void testSetGender()
+    {
+        stub.setGender("man");
+
+        assertEquals(Person.Gender.MAN, stub.getGender());
+    }
+
+    @Test
+    @DisplayName("Test getting mother when not set")
+    public void testGetMotherWhenNotSet()
+    {
+        assertNull(stub.getMother());
+    }
+
+    @Test
+    @DisplayName("Test setting mother")
+    public void testSetMother()
+    {
+        Person mother = new Person("Female Doe", "woman");
+        stub.setMother(mother);
+
+        assertEquals(mother, stub.getMother());
+    }
+
+    @Test
+    @DisplayName("Test setting self as mother")
+    public void testSetSelfAsMother()
+    {
+        assertThrows(IllegalArgumentException.class, () -> stub.setMother(stub));
+    }
+
+    @Test
+    @DisplayName("Test getting father when not set")
+    public void testGetFatherWhenNotSet()
+    {
+        assertNull(stub.getFather());
+    }
+
+    @Test
+    @DisplayName("Test setting father")
+    public void testSetFather()
+    {
+        Person father = new Person("Male Doe", "man");
+        stub.setFather(father);
+
+        assertEquals(father, stub.getFather());
+    }
+
+    @Test
+    @DisplayName("Test setting self as father")
+    public void testSetSelfAsFather()
+    {
+        assertThrows(IllegalArgumentException.class, () -> stub.setFather(stub));
+    }
+
+    @Test
+    @DisplayName("Test get children when empty")
+    public void testGetChildrenWhenEmpty()
+    {
+        assertEquals(0, stub.getChildren().size());
+    }
+
+    @Test
+    @DisplayName("Test adding child")
+    public void testAddChild()
+    {
+        Person child = new Person("Baby Doe", "man");
+        stub.addChild(child);
+
+        assertEquals(1, stub.getChildren().size());
+        assertTrue(stub.getChildren().contains(child));
+    }
+
+    @Test
+    @DisplayName("Test adding same child")
+    public void testAddSameChild()
+    {
+        Person child = new Person("Baby Doe", "man");
+        stub.addChild(child);
+
+        assertThrows(IllegalArgumentException.class, () -> stub.addChild(child));
+    }
+}
